c4dd4fce23c50628b0adf4c24ae425351fec1605
Dmitry Glushkov <50074706+inparabox@users.noreply.github.com>
2022-07-08 18:53:23 +0300

    обнулено

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,45 @@
 /*Задаем переменные всем элементам взаимодействия*/
 
-let popupEditProfile = document.querySelector('.popup__edit'); //  попап редактирования профиля
-let openPopupButton = document.querySelector('.open-popup'); // Кнопка редкатирования профиля 
-let closePopupButton = document.querySelector('.popup__btn-close'); // Крестик закрытия попапа для Edit profile
-let closePopupButtonAddForm = document.querySelector('.popup__btn-close-for-addCard'); // Крестик закрытия попапа для AddCard
-
+let popupEditProfile = document.querySelector(".popup__edit"); //  попап редактирования профиля
+let openPopupButton = document.querySelector(".open-popup"); // Кнопка редкатирования профиля
+let closePopupButton = document.querySelector(".popup__btn-close"); // Крестик закрытия попапа для Edit profile
+let closePopupButtonAddForm = document.querySelector(".popup__btn-close-for-addCard"); // Крестик закрытия попапа для AddCard
 
 /* Находим формы в полях данных из формы*/
 
-let formElement = document.querySelector(".popup__container");
+let EditFormElement = document.querySelector(".popup__container-editProfile"); // Форма Редактирвоания профиля
+let EditFormSaveButton = document.querySelector(".popup__btn-save-edit-form"); // Кнопка сохранения в редактиврования профиля
+
 let q1 = document.querySelector(".profile__name");
 let q2 = document.querySelector(".profile__mission");
 
 let nameInput = document.querySelector(".popup__name");
 let jobInput = document.querySelector(".popup__job");
 
+//вешаем обработчик и открываем поп-ап
 
-/* открываем поп-ап V1 */
-
-openPopupButton.addEventListener('click', openPopUp);
+openPopupButton.addEventListener("click", openPopUp);
 
 function openPopUp() {
-    popupEditProfile.classList.add('popup_opened');
-    nameInput.value =  q1.textContent;
-    jobInput.value = q2.textContent;    
-    console.log('привет');
-  };
- 
+  popupEditProfile.classList.add("popup_opened");
+  nameInput.value = q1.textContent;
+  jobInput.value = q2.textContent;
+  console.log("Это кнопка редактирования профиля");
+}
 
-/* открываем поп-ап V2
+// вешаем обработчик и Закрываем и сохраняем данные пользователя поп-ап
+
+closePopupButton.addEventListener("click", closePopUp);
+
+function closePopUp() {
+  popupEditProfile.classList.remove("popup_opened");
+  console.log("Это крестик закрытия формы редактирования профиля");
+}
+
+
+/*  V2 в эом случае функции невозможно переиспользовать
+
+// Открываем поп-ап V2
 
 openPopupButton.addEventListener('click',function(){
   popupEditProfile.classList.add('popup_opened');
@@ -36,22 +47,8 @@ openPopupButton.addEventListener('click',function(){
   jobInput.value = q2.textContent;    
 });
 
-*/
 
-
-/* Закрывакем поп-ап V1*/
-
-
-closePopupButton.addEventListener('click',closePopUp);
-
-function closePopUp() {
-  popupEditProfile.classList.remove('popup_opened');
-
-};
-
-
-
-/* Закрывакем поп-ап V2
+// Закрываем и сохраняем данные пользователя поп-ап V2
 
 closePopupButton.addEventListener('click',function(){
   popupEditProfile.classList.remove('popup_opened');
@@ -61,20 +58,16 @@ closePopupButton.addEventListener('click',function(){
 */
 
 
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
 
-// Выберите элементы, куда должны быть вставлены значения полей
-function formSubmitHandler (evt) {
+EditFormSaveButton.addEventListener("click", SaveUserDataInProfile); // новое 2
+
+function SaveUserDataInProfile(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   /*popupEditProfile.classList.remove('popup_opened');*/
   closePopUp();
   q1.textContent = nameInput.value;
-  q2.textContent = jobInput.value; 
+  q2.textContent = jobInput.value;
+  console.log("Это кнопка сохранения и отправики новых данных в форму");
 }
-
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
-
-
