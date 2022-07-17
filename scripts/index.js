42955a5a1acb358e16e524ae13a5620cfe574cd3
Dmitry Glushkov <50074706+inparabox@users.noreply.github.com>
2022-07-17 23:28:53 +0300

    удаление слушателей на элементах popup_недоделано на кнопке сохранить

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 //сделать кнопку закрытия на все элементы с одним CSS  - сейчас заведегны разные чтобы отслеживать действия
 
 
+const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector(".popup__edit"); //  попап редактирования профиля
+const popupAddNewPlace = document.querySelector('.popup__addCard'); //  попап добавления нового места
+
+const editProfileButton = document.querySelector(".open-popup"); // Кнопка редкатирования профиля
+const closePopupButton = document.querySelector(".popup__btn-close"); // Крестик закрытия попапа для Edit profile
 
 
-/*Задаем переменные всем элементам взаимодействия*/
 
-let popupEditProfile = document.querySelector(".popup__edit"); //  попап редактирования профиля
-let openPopupButton = document.querySelector(".open-popup"); // Кнопка редкатирования профиля
-let closePopupButton = document.querySelector(".popup__btn-close"); // Крестик закрытия попапа для Edit profile
 //let closePopupButtonAddForm = document.querySelector(".popup__btn-close-for-addCard"); // Крестик закрытия попапа для AddCard
 
 /* Находим формы в полях данных из формы*/
@@ -24,9 +26,10 @@ const templateBox = document.querySelector('#template-box').content;
 const element = templateBox.querySelector('.element');
 const elements = document.querySelector('.elements');
 
+const addElementButton = document.querySelector('.profile__button-add');
 
-const popupAddNewPlace = document.querySelector('.popup__addCard'); //  попап добавления нового места
-const buttonAddNew = document.querySelector('.profile__button-add');
+
+
 const buttonClosePopUpAddNew = document.querySelector('.popup__btn-close-for-addCard');
 
 
@@ -45,120 +48,62 @@ const popupCaption = document.querySelector('.popup__figcaption');
 const container = document.querySelector('.popup__container');
 
 
-//вешаем обработчик и открываем поп-ап
-
-const popup = document.querySelector('.popup');
 
 
 
 
-openPopupButton.addEventListener("click", openPopUp);
+editProfileButton.addEventListener("click", openPopUp);
 
 function openPopUp() {
-  //popupEditProfile.classList.add("popup_opened");//работает
   popup.classList.add("popup_opened");
-
-
+  popup.querySelector('.popup__btn-close').addEventListener('click', closePopup);
   nameInput.value = q1.textContent;
   jobInput.value = q2.textContent;
   console.log("Это кнопка редактирования профиля");
 }
 
+//closePopupButton.addEventListener("click", closePopup); // обработчик убран в функцию открытия, чтобы не утекала память
+
+
+
+
+
+
+
+
 
 
 /*
-// Это не работает
-
-const openPopUp = (item, e) => {
-  let popup = document.querySelector('.popup__edit');
-  if(item.link) {
-    popup = document.querySelector('.popup__photo');
-    popup.querySelector('.element__image').src = item.link;
-  }
-  popup.querySelector('.popup__title').textContent = item.name;
-  popup.classList.add('popup_opened');
-  popup.querySelector('.popup__btn-close').addEventListener('click', closePopUp);
-  //popup.addEventListener('click', closePopupByOverlay);
+function closePopup() {
+  popup.classList.remove("popup_opened");
+  console.log("закрыли форму редактирования профиля");
 }
-
-
 */
 
 
-
-
-
-
-
-
-
-
-
-
-
-// вешаем обработчик и Закрываем и сохраняем данные пользователя поп-ап
-
-
-
-
-closePopupButton.addEventListener("click", closePopUp);
-function closePopUp() {
-
-  popupEditProfile.classList.remove("popup_opened");//работает
-  //popupPhoto.classList.remove("popup_opened");// это убрать потом
-
-  console.log("Это крестик закрытия формы редактирования профиля");
-}
-
-
-
-/*
-Это не работает
-
-const closePopUp = (e) => {
-  const popup = e.target.closest('.popup');
+const closePopup = (evt) => {
+  const popup = evt.target.closest('.popup');
   popup.classList.remove('popup_opened');
-  popup.querySelector('.popup__btn-close').removeEventListener('click', closePopUp);
-  //popup.removeEventListener('click', closePopupByOverlay);
+  popup.querySelector('.popup__btn-close').removeEventListener('click', closePopup);
 }
 
-*/
 
 
 
 
 
 
-/*  V2 в эом случае функции невозможно переиспользовать
-
-// Открываем поп-ап V2
-
-openPopupButton.addEventListener('click',function(){
-  popupEditProfile.classList.add('popup_opened');
-  nameInput.value =  q1.textContent;
-  jobInput.value = q2.textContent;    
-});
 
 
-// Закрываем и сохраняем данные пользователя поп-ап V2
-
-closePopupButton.addEventListener('click',function(){
-  popupEditProfile.classList.remove('popup_opened');
-
-})
-
-*/
 
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 
 EditFormSaveButton.addEventListener("click", SaveUserDataInProfile); // новое 2
 
 function SaveUserDataInProfile(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  //popupEditProfile.classList.remove('popup_opened');//работает
-  closePopUp();
+  closePopup();
   q1.textContent = nameInput.value;
   q2.textContent = jobInput.value;
   console.log("Это кнопка сохранения и отправики новых данных в форму");
@@ -242,6 +187,7 @@ function cloneCard(item) {
   const name = newBox.querySelector(".element__text");
   name.textContent = item.name;
   link.src = item.link;
+  link.alt = item.name;
   
   
   // подписка на события
@@ -252,16 +198,10 @@ function cloneCard(item) {
     popupImage.src = link.src;
     popupCaption.textContent = item.name;
     popupPhoto.classList.add("popup_opened");
-
   })
 
   return newBox;
 }
-
-
-
-
-
 
 function createUserCard() {
   
@@ -272,61 +212,48 @@ function createUserCard() {
   }
   const userCard = cloneCard(item);
   elements.prepend(userCard);
-
 }
-
-
-
-
-
-
-
 
 
 // вставляем 6 карточек
 
 initialCards.forEach((item) => {
-  //создаем карточку
-  const sixElements = cloneCard(item);
-  //встраиваем ее в dom
-  elements.prepend(sixElements);
+  const sixElements = cloneCard(item); //создаем переменную для элементов
+  elements.prepend(sixElements); //встраиваем в dom
 });
-
-
-
-
-
 
 //Открываем PopUp ADD NEW PLACE
 
-
-buttonAddNew.addEventListener('click', (evt) => {
+addElementButton.addEventListener('click', (evt) => {
   console.log('Хочешь добавить новое фото');
-  openPopUpAddNewPlace();//работает
-
-
+  openPopUpAddNewPlace();
 })
+
 
 // функция открытия поп-апа ADD NEW PLACE
 function openPopUpAddNewPlace() {
   popupAddNewPlace.classList.add("popup_opened");
+  popupAddNewPlace.querySelector('.popup__btn-close').addEventListener('click', closePopup);
+
 }
 
+/*
+// Обработчик и функция закртыия поп-ап ADD NEW PLACE - обработчик засунул в функцию открытия + функция одна
 
-//Закрываем поп-ап ADD NEW PLACE
 buttonClosePopUpAddNew.addEventListener('click', (evt) => {
   console.log('Закртываем поп-ап ADD NEW');
-  closePopUpAddNewPlace(); //работает
-
+  //closePopUpAddNewPlace(); //работает
+  closePopup();
 })
-
-
 
 // функция закрытия поп-апа ADD NEW PLACE
 function closePopUpAddNewPlace() {
   popupAddNewPlace.classList.remove("popup_opened");
-
 }
+
+*/
+
+
 
 
 
@@ -338,7 +265,8 @@ function addUserPlace(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   console.log('Кнопка Сохранить и закрываем поп-ап ADD NEW');
   createUserCard();
-  closePopUpAddNewPlace();  
+  //closePopUpAddNewPlace();  
+  closePopup();
 }
 
 //очистка формы
@@ -351,13 +279,13 @@ function clearForm (evt) {
   document.querySelector('.popup__form-add-card').reset();
 }
 
-//закрашиваем like
+//закрашиваем like при событии клик
 
 function like (evt) {
   evt.target.classList.toggle('element__button-like-active');
 }
 
-//basket
+//basket - удаление родителя при клике на коризну элемента
 
 function basket(evt) {
   console.log('Тыкнул на корзину');
