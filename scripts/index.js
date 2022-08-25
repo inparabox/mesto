77a73aab18a89c40c5f76e4634445d1c314509f5
Dmitry Glushkov <50074706+inparabox@users.noreply.github.com>
2022-08-25 18:53:43 +0300

    fix: функция openpopup теперь универсальна

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,6 @@ const profileName = document.querySelector(".profile__name");
 const profileMission = document.querySelector(".profile__mission");
 const profileButtonEdit = document
   .querySelector(".profile__button-edit")
-  .addEventListener("click", EditProfile);
 const popupBtnSave = document.querySelector(".popup__btn-save");
 const popupName = document.querySelector(".popup__name");
 const popupJob = document.querySelector(".popup__job");
@@ -14,15 +13,12 @@ const element = templateBox.querySelector(".element");
 const elements = document.querySelector(".elements");
 const btnAddNewElement = document
   .querySelector(".profile__button-add")
-  .addEventListener("click", openPopUpAddNew);
 const popupAddCard = document.querySelector(".popup_addCard");
 const popupPlaceName = document.querySelector(".popup__place-name");
 const popupPlaceLink = document.querySelector(".popup__place-link");
 const popupPhoto = document.querySelector(".popup_photo");
 const popupImage = popupPhoto.querySelector(".popup__image");
 const popupCaption = document.querySelector('.popup__figcaption');
-
-
 
 const initialCards = [
   {
@@ -51,26 +47,27 @@ const initialCards = [
   },
 ];
 
-function EditProfile() {
-  popupEdit.classList.add("popup_opened");
-  popupBtnClose.addEventListener("click", closePopup);
-  popupName.value = profileName.textContent;
-  popupJob.value = profileMission.textContent;
-  popupBtnSave.addEventListener("click", saveProfileData);
-  console.log("Редактирование профиля");
+function openPopup(popupElement) {
+  popupElement.classList.add("popup_opened");
 }
 
 function closePopup(evt) {
   const popup = evt.target.closest(".popup");
   popup.classList.remove("popup_opened");
-  console.log("Закрыть попап");
 }
+
+profileButtonEdit.addEventListener('click', function() {
+  openPopup(popupEdit);
+  popupBtnClose.addEventListener("click", closePopup);
+  popupName.value = profileName.textContent;
+  popupJob.value = profileMission.textContent;
+  popupBtnSave.addEventListener("click", saveProfileData);
+})
 
 function saveProfileData(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileMission.textContent = popupJob.value;
-  console.log("Сохранить новые даные");
   closePopup(evt);
 }
 
@@ -86,37 +83,32 @@ function cloneCard(item) {
     .querySelector(".element__button-like")
     .addEventListener("click", likeNolike);
   function likeNolike() {
-    console.log("Сердечко");
     newBox
       .querySelector(".element__button-like")
       .classList.toggle("element__button-like-active");
   }
-
   newBox
     .querySelector(".element__delete-button")
     .addEventListener("click", deleteCard);
   newBox
     .querySelector(".element__image")
     .addEventListener("click", zoom);
-
   return newBox;
 }
-
 initialCards.forEach((item) => {
   const sixElements = cloneCard(item);
   elements.prepend(sixElements);
 });
 
-function openPopUpAddNew() {
-  popupAddCard.classList.add("popup_opened");
+btnAddNewElement.addEventListener("click", function() {
+  openPopup(popupAddCard);
   popupAddCard
     .querySelector(".popup__btn-close")
     .addEventListener("click", closePopup);
   popupAddCard
     .querySelector(".popup__btn-save")
     .addEventListener("click", addNewElement);
-  console.log("Попап добавления нового элемента открыт");
-}
+});
 
 function addNewElement(evt) {
   evt.preventDefault();
@@ -131,14 +123,10 @@ function addNewElement(evt) {
   popupAddCard
     .querySelector(".popup__btn-save")
     .addEventListener("click", clearForm);
-  console.log(
-    "Кнопка Сохранить: закрываем поп-ап ADD NEW + добавляем новый элемент"
-  );
 }
 
 function clearForm(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  console.log("Очистка формы");
   document.querySelector(".popup__form-add-card").reset();
 }
 
@@ -155,5 +143,4 @@ function zoom(evt) {
     .addEventListener("click", closePopup);
   popupImage.src = evt.target.closest(".element__image").src;
   popupCaption.textContent = evt.target.closest(".element__image").alt;
-  console.log("Тыкнул на фото");
 }
